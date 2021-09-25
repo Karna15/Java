5cf369aa175d68f628c724f1e4cfdfdf29ff84bb
Karna15 <karnarooney15@gmail.com>
2021-09-25 16:37:35 +0530

    Files java

---
+++ Programs/FileReadWrite/ReadFileList.java
@@ -0,0 +1,21 @@
+package Programs.FileReadWrite;
+
+import java.io.File;
+
+public class ReadFileList {
+
+    public static void main(String[] args) {
+
+	File directoryPath = new File("G:\\Documents\\");
+
+	String listofFiles[] = directoryPath.list();
+	File[] FilewithFolder = directoryPath.listFiles();
+	System.out.println("List of files in the specified directory:");
+	for (int i = 0; i < listofFiles.length || i < FilewithFolder.length; i++) {
+	    System.out.println(listofFiles[i]); // displays file name
+	    System.out.println(FilewithFolder[i]); // displays folder and file name
+	}
+
+    }
+
+}
