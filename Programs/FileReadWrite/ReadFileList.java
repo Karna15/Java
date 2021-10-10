8781de46a4024c26c54f77534a8f2ff699f6b67e
Karna15 <karnarooney15@gmail.com>
2021-10-10 18:50:38 +0530

    Update

--- Programs/FileReadWrite/ReadFileList.java
+++ Programs/FileReadWrite/ReadFileList.java
@@ -4,18 +4,17 @@ import java.io.File;
 
 public class ReadFileList {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	File directoryPath = new File("G:\\Documents\\");
+		File directoryPath = new File("G:\\Documents");
 
-	String listofFiles[] = directoryPath.list();
-	File[] FilewithFolder = directoryPath.listFiles();
-	System.out.println("List of files in the specified directory:");
-	for (int i = 0; i < listofFiles.length || i < FilewithFolder.length; i++) {
-	    System.out.println(listofFiles[i]); // displays file name
-	    System.out.println(FilewithFolder[i]); // displays folder and file name
+		String listofFiles[] = directoryPath.list();
+		File[] FilewithFolder = directoryPath.listFiles();
+		System.out.println("List of files in the specified directory:");
+		for (int i = 0; i < listofFiles.length || i < FilewithFolder.length; i++) {
+			System.out.println(listofFiles[i]); // displays file name
+			System.out.println(FilewithFolder[i]); // displays folder and file name
+		}
 	}
-
-    }
 
 }
